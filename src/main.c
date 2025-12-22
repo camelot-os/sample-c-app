43bf83f6cb876ebdc4d618f79e34e8db35e8e3fc
Philippe Thierry <phil@reseau-libre.net>
2025-12-22 09:35:55 +0100

    update: adding loop+sleep support to sample C app

--- src/main.c
+++ src/main.c
@@ -5,9 +5,20 @@
  */
 
 #include <stdio.h>
+#include <uapi.h>
+#include <types.h>
 
 int main(void)
 {
-    printf("Hello, World from sample C app\n");
+    SleepDuration duration;
+    uint32_t sleep_time = 1000UL;
+    duration.tag = SLEEP_DURATION_ARBITRARY_MS;
+    duration.arbitrary_ms = sleep_time;
+
+
+    while (1) {
+    	printf("Hello, World from sample C app\n");
+    	__sys_sleep(duration, SLEEP_MODE_DEEP);
+    }
     return 0;
 }
