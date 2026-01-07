878542a6779f052bb5cb544afeaad081ea1fa012
Philippe Thierry <phil@reseau-libre.net>
2026-01-07 10:24:53 +0100

    sha: adding finalize, sha256 calculation ok

--- src/main.c
+++ src/main.c
@@ -9,19 +9,38 @@
 #include <types.h>
 #include <libecc/hash/sha256.h>
 
+void hexdump(const u8 *buf, u16 buflen)
+{
+	u32 i;
+
+	if (buf == NULL) {
+		goto err;
+	}
+	for (i = 0; i < (u32)buflen; i++) {
+		printf("%02x", buf[i]);
+	}
+	printf("\n");
+
+err:
+	return;
+}
+
 int main(void)
 {
     SleepDuration duration;
     uint32_t sleep_time = 1000UL;
     duration.tag = SLEEP_DURATION_ARBITRARY_MS;
     duration.arbitrary_ms = sleep_time;
+    uint8_t sha256_buffer[32] = {0};
 
     const char *hello_msg = "Hello, World from sample C app\n";
     sha256_context ctx;
     sha256_init(&ctx);
     sha256_update(&ctx, hello_msg, strlen(hello_msg));
+    sha256_final(&ctx, sha256_buffer);
     while (1) {
     	printf(hello_msg);
+        hexdump(ctx.sha256_buffer, 32);
     	__sys_sleep(duration, SLEEP_MODE_DEEP);
     }
     return 0;
